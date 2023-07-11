39bac68d313286dad2eeab8df9a0a3b99fec4ac3
Jasmine Lim <jasmine_lim13@yahoo.com>
2023-07-11 16:20:56 -0500

    controllers
    git commit -m controllers
    '

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,4 +1,22 @@
 package org.launchcode.hellospring.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HelloController {
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello, Spring!";
+    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+
 }
