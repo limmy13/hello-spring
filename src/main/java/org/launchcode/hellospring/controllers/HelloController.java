61fc4818b92b7b18b7dc66ee63a823ef02d3fb92
Jasmine Lim <jasmine_lim13@yahoo.com>
2023-07-11 16:31:12 -0500

    path param

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,6 +2,7 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -30,6 +31,10 @@ public class HelloController {
     }
 
     // handles requests of the form /hello/LaunchCode
-
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
 
 }
